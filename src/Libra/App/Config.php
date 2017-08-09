de150e9412a70aa04d6c5e8dca35ad4f1a2a89d6
Josh Freeman <josh.freeman@extreme-creations.co.uk>
2017-08-09 12:50:31 +0100

    lower case const

--- src/Libra/App/Config.php
+++ src/Libra/App/Config.php
@@ -9,6 +9,6 @@ namespace Libra\App;
  */
 class Config
 {
-    CONST SQLITE_FILE = __DIR__ . '/../../../db/app_data.sqlite';
+    const SQLITE_FILE = __DIR__ . '/../../../db/app_data.sqlite';
     const DUMP_PATH = __DIR__.'/../../../dump';
 }
